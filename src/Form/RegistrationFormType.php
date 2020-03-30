510ef0c1e960792cfbd6abbd7bc01228308de857
Sorin Loghin <sorin.loghin@in2code.de>
2020-03-30 23:52:36 +0200

    [CLEANUP] Code reformat

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,11 +4,9 @@ namespace Corohelp\Form;
 
 use Corohelp\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -33,8 +31,7 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
