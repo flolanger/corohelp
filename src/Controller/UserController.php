932c20d3a2b8b33ccb35a6995c577971128a1dc6
Sorin Loghin <sorin.loghin@in2code.de>
2020-04-02 23:53:00 +0200

    [TASK] Add action and type to edit user

---
+++ src/Controller/UserController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Corohelp\Controller;
+
+use Corohelp\Entity\User;
+use Corohelp\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class UserController
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @param Request $request
+     * @param User $user
+     * @return Response
+     */
+    public function edit(Request $request, User $user): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('category_index');
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @param User $user
+     * @return Response
+     */
+    public function delete(Request $request, User $user): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('login');
+    }
+}
