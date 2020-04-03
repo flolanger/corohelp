7fd44f74eb76a0616be34c8a036790ed8f027f07
Sorin Loghin <sorin.loghin@in2code.de>
2020-04-03 21:28:31 +0200

    [TASK] Add autocomplete for location field

---
+++ public/js/basic.js
@@ -0,0 +1,47 @@
+let input = document.getElementById('registration_location');
+input.addEventListener('keydown', function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+    }
+});
+
+const URL = 'https://autocomplete.geocoder.ls.hereapi.com/6.2/suggest.json';
+const APIKEY = 'knEa6xpWyfBx1iRl1C7RRA6potNpUUYZORhYd4l1Tnk';
+
+autocomplete({
+    input: input,
+    fetch: function (text, update) {
+        text = text.toLowerCase();
+
+        let response = getJSON(
+            URL + '?apiKey=' + APIKEY + '&country=DEU,AUT&resultType=city&query=' + text,
+            function (err, data) {
+                let suggestions = [];
+                for (let i = 0; i < data.suggestions.length; i++) {
+                    let suggestion = {};
+                    suggestion.label = data.suggestions[i].address.city;
+                    suggestion.value = data.suggestions[i].address.city;
+                    suggestions.push(suggestion);
+                }
+                update(suggestions);
+            });
+    },
+    onSelect: function (item) {
+        input.value = item.label;
+    }
+});
+
+let getJSON = function (url, callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+        let status = xhr.status;
+        if (status === 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status, xhr.response);
+        }
+    };
+    xhr.send();
+};
